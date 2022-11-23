5ad1e1a44123a57ea58b7c084d9440bc5381387e
dhruv2253 <dhruv225@outlook.com>
2022-11-23 15:27:49 -0500

    developed functions and more event listeners

--- main.js
+++ main.js
@@ -23,47 +23,79 @@ let currentDisplay = document.querySelector('.currentOperations');
 let previousDisplay = document.querySelector('.previousOperations');
 const clearButton = document.querySelector('.clear');
 const equalButton = document.querySelector('.equal');
-let firstOperand;
-let secondOperand;
-let currentOperation;
+const delButton = document.querySelector('.del');
+const periodButton = document.querySelector('.period');
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false;
 function clear() {
     currentDisplay.textContent = "0";
-    previousDisplay.textContent = "";
+    previousDisplay.innerHTML = '&nbsp;';
     firstOperand = ''
     secondOperand = ''
-    currentOperation = ''
+    currentOperation = null;
 }
 
 //Clear Button function
 clearButton.addEventListener('click', clear);
-
+delButton.addEventListener('click', del);
+periodButton.addEventListener('click', appendPeriod);
+function appendNumber(num) {
+    if (currentDisplay.textContent === '0' || shouldResetScreen)
+        resetScreen();
+    currentDisplay.textContent += num;
+}
 
 //Number buttons
-numButtons.forEach(button => button.addEventListener('click', () => {
-    currentDisplay = document.querySelector('.currentOperations')
-    console.log(button.textContent)
-    currentDisplay.textContent += button.textContent
+numButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent) 
+        )
+);
 
-}));
+function resetScreen() {
+    currentDisplay.textContent = '';
+    shouldResetScreen = false;
+  }
 
 
+function del() {
+    currentDisplay.textContent = currentDisplay.textContent.toString().slice(0, -1);
+}
+
+function appendPeriod(){
+    if (shouldResetScreen) resetScreen()
+    if (currentDisplay.textContent.includes('.'))
+        return;
+    currentDisplay.textContent += '.';
+}
 
 operationButtons.forEach((button) => button.addEventListener('click', () => setOperation(button.textContent)));
 
 equalButton.addEventListener('click', eval);
  
 function eval() {
-    if (currentOperation === null) return
+    if (currentOperation === null || shouldResetScreen) return
+    if (currentOperation === '/' && currentDisplay.textContent === '0'){
+        alert("Cannot divide by 0")
+        return
+    }
     secondOperand = currentDisplay.textContent;
-    currentDisplay.textContent = operate(currentOperation, parseInt(firstOperand), parseInt(secondOperand));
+    currentDisplay.textContent = roundR(operate(currentOperation, parseFloat(firstOperand), parseFloat(secondOperand)));
     previousDisplay.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
     currentOperation = null;
+
+}
+
+function roundR(num) {
+    return Math.round(num * 1000) / 1000;
 }
 
 function setOperation(operator) {
-    if (currentOperation === null) return
+    if (currentOperation !== null)
+         eval()
     firstOperand = currentDisplay.textContent;
     currentOperation = operator;
     previousDisplay.textContent = `${firstOperand} ${currentOperation}`;
-    currentDisplay.textContent = "";
+    shouldResetScreen = true;
 }
